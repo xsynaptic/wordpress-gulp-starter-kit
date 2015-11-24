be4884b2283077cc0c3281244a37f34979e34404
Alexander Synaptic <alexander@synapticism.com>
2015-11-24 16:40:23 +0800

    Removing unnecessary HTML markup

--- src/header.php
+++ src/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <title><?php wp_title( '-', true, 'right' ); ?></title>
@@ -12,13 +12,13 @@
 <body <?php body_class(); ?>>
   <div id="page" class="hfeed site">
     <div id="wrap-header" class="wrap-header">
-      <header id="masthead" class="site-header" role="banner">
+      <header id="masthead" class="site-header">
         <div class="site-branding">
           <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
           <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
         </div>
-        <nav id="site-navigation" class="site-navigation" role="navigation">
-          <button id="responsive-menu-toggle" role="button"><?php _e( 'Menu', 'voidx' ); ?></button>
+        <nav id="site-navigation" class="site-navigation">
+          <button id="responsive-menu-toggle"><?php _e( 'Menu', 'voidx' ); ?></button>
           <div id="responsive-menu"><?php wp_nav_menu( array( 'theme_location' => 'header', 'menu_id' => 'menu-header', 'menu_class' => 'menu-inline' ) ); ?></div>
         </nav>
       </header>
