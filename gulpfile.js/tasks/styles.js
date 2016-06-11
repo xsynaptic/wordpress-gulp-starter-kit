2a30db3f6cb59b7157fcca157eed9742d9b8e698
Alexander Synaptic <alexander@synapticism.com>
2016-06-11 19:03:55 +0800

    Proper cssnano integration

--- gulpfile.js/tasks/styles.js
+++ gulpfile.js/tasks/styles.js
@@ -4,29 +4,25 @@ var gulp          = require('gulp')
   , gutil         = require('gulp-util')
   , plugins       = require('gulp-load-plugins')({ camelize: true })
   , config        = require('../../gulpconfig').styles
-  , autoprefixer  = require('autoprefixer')
-  , processors    = [autoprefixer(config.autoprefixer)] // Add additional PostCSS plugins to this array as needed
 ;
 
-// Build stylesheets from source Sass files, autoprefix, and write source maps (for debugging) with rubySass
+// Build stylesheets from source Sass files, post-process, and write source maps (for debugging) with rubySass
 gulp.task('styles-rubysass', function() {
   return plugins.rubySass(config.build.src, config.rubySass)
   .on('error', gutil.log) // Log errors instead of killing the process
-  .pipe(plugins.postcss(processors))
-  .pipe(plugins.cssnano(config.minify))
-  .pipe(plugins.sourcemaps.write('./')) // No need to init; this is set in the configuration
-  .pipe(gulp.dest(config.build.dest)); // Drops the unminified CSS file into the `build` folder
+  .pipe(plugins.cssnano(config.cssnano)) // Post-process CSS (minify, autoprefix, etc.)
+  .pipe(plugins.sourcemaps.write('./')) // Write an external sourcemap; no need to initialize with rubysass as this is already done in the configuration
+  .pipe(gulp.dest(config.build.dest)); // Render the final CSS file(s) into the `build` folder
 });
 
-// Build stylesheets from source Sass files, autoprefix, and write source maps (for debugging) with libsass
+// Build stylesheets from source Sass files, post-process, and write source maps (for debugging) with libsass
 gulp.task('styles-libsass', function() {
   return gulp.src(config.build.src)
-  .pipe(plugins.sourcemaps.init())
+  .pipe(plugins.sourcemaps.init()) // Note that sourcemaps need to be initialized with libsass
   .pipe(plugins.sass(config.libsass))
-  .pipe(plugins.postcss(processors))
-  .pipe(plugins.cssnano(config.minify))
-  .pipe(plugins.sourcemaps.write('./')) // Writes an external sourcemap
-  .pipe(gulp.dest(config.build.dest)); // Drops the unminified CSS file into the `build` folder
+  .pipe(plugins.cssnano(config.cssnano))
+  .pipe(plugins.sourcemaps.write('./'))
+  .pipe(gulp.dest(config.build.dest));
 });
 
 // Easily configure the Sass compiler from `/gulpconfig.js`
